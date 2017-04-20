0e703095ead47b9614c348288aaad5d3663fdcdd
Wojtek Mazur <zgnitek123.sbb@gmail.com>
2017-04-20 23:20:25 +0200

    Po napisaniu

--- test.py
+++ test.py
@@ -35,3 +35,13 @@ for iter in range(15000):
     #forward propagation
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
+
+    # how much did we miss
+    l1_error = y - l1
+
+    l1_delta = l1_error * nonlin(l1, True)
+
+    syn0 += np.dot(l0.T, l1_delta)
+
+print("Output after training: ")
+print(l1)
