ea6a19e21edbcb6a1ad3268d8667ecbd9ff4af08
Wojtek Mazur <zgnitek123.sbb@gmail.com>
2017-05-03 21:02:28 +0200

    third part of sentdex tutorial

---
+++ strings.py
@@ -0,0 +1,30 @@
+names = ['Jeff', 'Gary', 'Jill', 'Samantha']
+
+# for name in names:
+#     print('Hello there, ' + name)
+#     print(' '.join(['Hello there,', name]))
+
+
+# print(', '.join(names))
+
+
+
+#E:\Dokumenty\Python projekty
+
+# import os
+# ##NOT RIHT WAY TO GET AWAY WITH PATHS
+# location_of_files = 'E:\\Dokumenty\\Python projekty'
+# file_name = 'example.txt'
+#
+# #print(location_of_files + '\\' file_name)
+# 
+# with open(os.path.join(location_of_files, file_name)) as f:
+#     print(f.read())
+
+
+
+
+# who = 'Gary'
+# how_many = 12
+#
+# print('{} bought {} apples today!'.format(who, how_many))
